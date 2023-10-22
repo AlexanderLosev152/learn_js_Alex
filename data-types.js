bf8fa8befbae9ac219cb2c9708675a4c8439d1d4
Alexander <losev.pavlovo@mail.ru>
2023-10-22 23:15:16 +0300

    first commit

---
+++ data-types.js
@@ -0,0 +1,25 @@
+// const vs let
+// const nameUser = "Alex";
+// let ageUser = 37;
+
+// ========================
+
+// data types
+const name = "Alex"; // string
+const age = 37; // number
+const isProgrammer = true; //boolean
+const cars = ["bmw", "mersedes", 29]; // array
+const person = {
+  name: "Alex",
+  age: 37,
+  isProgrammer: true,
+  cars: ["bmw", "mersedes", 29],
+}; // object
+const person2 = {
+  name,
+  age,
+  isProgrammer,
+  cars,
+};
+const random = undefined; // undefined
+const nall = null; //
