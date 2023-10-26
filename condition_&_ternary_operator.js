3af1f70a70baa8365790853ba906fcd9724b21e7
Alexander <losev.pavlovo@mail.ru>
2023-10-26 23:28:52 +0300

    26/10

--- condition_&_ternary_operator.js
+++ condition_&_ternary_operator.js
@@ -37,6 +37,20 @@ else if (scoops < 2) {
 }
 // ========================
 
+const word = "bottles";
+let count = 99;
+while (count > 0) {
+  console.log(`${count} ${word} of the beer on the wall`);
+  console.log(`${count} ${word} of beer`);
+  console.log("Take one down , pass it around");
+  count -= 1;
+  if (count > 0) {
+    console.log(`${count} ${word} of the beer on the wall`);
+  } else {
+    console.log(`No more ${word} of the beer on the wall`);
+  }
+}
+
 // function getStatus(age) {
 //   if (age < 14) return "Children";
 //   if (age < 20) return "Teenager";
--- function.js
+++ function.js
@@ -14,3 +14,32 @@ const getPrice = (name) => {
 };
 
 console.log("$", getPrice("Lada"));
+
+// =================
+
+// const dogName = "rover";
+// const dogWeight = 23;
+
+// const dogName = "spot";
+// const dogWeight = 13;
+
+// const dogName = "spice";
+// const dogWeight = 53;
+
+// const dogName = "lady";
+// const dogWeight = 17;
+
+// const weight = (dogName, dogWeight) => {
+//   if (dogWeight > 20) {
+//     console.log(`${dogName} says WOOF WOOF`);
+//   } else {
+//     console.log(`${dogName} says woof woof`);
+//   }
+// };
+
+// console.log(weight("rover", 23));
+// console.log(weight("spot", 13));
+// console.log(weight("spice", 53));
+// console.log(weight("lady", 17));
+
+// =================
